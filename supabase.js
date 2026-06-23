53ad350302dfe2cca99b06ec6d0df516c916029c
Wadan68 <outmanout@gmail.com>
2026-06-23 19:15:25 +0800

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+'https://kfssijcpnjzyrcpwtecq.supabase.co';
+
+const SUPABASE_KEY =
+'sb_publishable_OtDfEmYVql6POECc_3ZI-w_smSpm_bD';
+
+const supabaseClient =
+supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
